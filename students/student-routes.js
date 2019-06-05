862ec9480b2b2c6d8b5cd0f02f2c98b5f9de2d77
Seth Lachman <lachmanseth17@gmail.com>
2019-06-05 17:03:37 -0400

    Added post method to students route.

--- students/student-routes.js
+++ students/student-routes.js
@@ -2,6 +2,16 @@ const router = require("express").Router();
 
 const Students = require("./students-model");
 
+router.post("/", validateStudentInfo, (req, res) => {
+  Students.add(req.body)
+    .then(student => {
+      res.status(200).json(student);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error adding student" });
+    });
+});
+
 router.get("/", (req, res) => {
   Students.find()
     .then(students => {
@@ -11,5 +21,15 @@ router.get("/", (req, res) => {
       res.status(500).json({ message: "Error retrieving students" });
     });
 });
+
+function validateStudentInfo(req, res, next) {
+  if (!req.body.name) {
+    res.status(400).json({ message: "Missing required name field" });
+  } else if (!req.body.cohort_id) {
+    res.status(400).json({ message: "Missing required cohort_id field" });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
